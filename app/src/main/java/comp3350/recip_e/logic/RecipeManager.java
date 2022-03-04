c17bd36aaac5652c79560921599d4203e3df5416
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2022-03-04 10:55:16 -0600

    Update Recipe Manager with Data Functions
    Add data specific functions to recipe manager.

--- app/src/main/java/comp3350/recip_e/logic/RecipeManager.java
+++ app/src/main/java/comp3350/recip_e/logic/RecipeManager.java
@@ -1,35 +1,34 @@
 package comp3350.recip_e.logic;
 
-import java.util.List;
+import comp3350.recip_e.application.Services;
+
+import com.comp3350.recip_e.database.recipeManager;
 
 import comp3350.recip_e.objects.Recipe;
 
 public class RecipeManager {
-    // TODO Store a pointer to the database interface
+    private recipeManager database;
 
     public RecipeManager() {
-        // TODO Save database pointer
+        database = Services.getRecipePersistence();
     }
 
     /**
-     * Gets a list of all recipes in the database
-     *
-     * @return A list recipes in the database
+     * Get the first recipe from the database
+     * @return  The first recipe in the database
      */
-    public List<Recipe> getAllRecipes() {
-        // TODO Call database function to get all recipes
-        return null;
+    public Recipe getFirstRecipe() {
+        return getRecipe(0);
     }
 
     /**
      * Gets a single recipe from the database
      *
-     * @param id ID of the recipe to get
-     * @return The recipe with the given ID
+     * @param id id of the recipe to get
+     * @return The recipe with the given id
      */
     public Recipe getRecipe(int id) {
-        // TODO Call database function to get a recipe by ID
-        return null;
+        return database.getRecipe(id, true);
     }
 
     /**
@@ -38,15 +37,15 @@ public class RecipeManager {
      * @param recipe The recipe to add
      */
     public void addRecipe(Recipe recipe) {
-        // TODO Call database function to add a recipe
+        database.addRecipe(recipe);
     }
 
     /**
      * Deletes a recipe from the database
      *
-     * @param id The ID of the recipe to delete
+     * @param id The id of the recipe to delete
      */
     public void deleteRecipe(int id) {
-        // TODO Call database function to delete the recipe with given ID
+        database.delRecipe(id);
     }
 }
