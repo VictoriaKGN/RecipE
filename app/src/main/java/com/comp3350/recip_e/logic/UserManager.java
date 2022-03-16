d5ba2dc1265fe336042c104e6909f9ff5ba62de9
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2022-03-16 11:42:18 -0500

    Create User Manager
    Create a user manager class with temporary stub functions.

---
+++ app/src/main/java/com/comp3350/recip_e/logic/UserManager.java
@@ -0,0 +1,41 @@
+package com.comp3350.recip_e.logic;
+
+import com.comp3350.recip_e.application.Services;
+import com.comp3350.recip_e.objects.User;
+
+public class UserManager {
+    // TODO Instance of User database
+
+    public UserManager() {
+        // TODO Set instance of database user
+    }
+
+    /**
+     * Get a single user from the database
+     *
+     * @param id id of the user to get
+     * @return The user with the given id
+     */
+    public User getUser(int id) {
+        // TODO Get user from database
+        return null;
+    }
+
+    /**
+     * Add a user to the database
+     *
+     * @param user The user to add
+     */
+    public void addUser(User user) {
+        // TODO Add user to database
+    }
+
+    /**
+     * Update a user in the database
+     *
+     * @param user  User with updated values
+     */
+    public void updateUser(User user) {
+        // TODO Update user in database
+    }
+}
