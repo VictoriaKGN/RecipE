2ee65f686df56157a6b9f047fba73df7ac8657c6
Br0k3N-ET3rNaL <tyliewilie99@gmail.com>
2022-03-16 11:41:39 -0500

    Create User Object
    Create an object representing a user, along with associated tests.

---
+++ app/src/main/java/com/comp3350/recip_e/objects/User.java
@@ -0,0 +1,29 @@
+package com.comp3350.recip_e.objects;
+
+public class User {
+    private final String email;
+    private final String username;
+    private String password;
+
+    public User(String email, String username, String password) {
+        this.email = email;
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
