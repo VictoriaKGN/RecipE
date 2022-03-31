2139b19006f1ff6537bab8db8fec7839ab01c9d2
VickyKPower <koganv@myumanitoba.ca>
2022-03-30 23:39:55 -0500

    remove logged user when logging out

--- app/src/main/java/com/comp3350/recip_e/UI/ProfileActivity.java
+++ app/src/main/java/com/comp3350/recip_e/UI/ProfileActivity.java
@@ -278,6 +278,7 @@ public class ProfileActivity extends AppCompatActivity {
             @Override
             public void onClick(View view)
             {
+                ((App)getApplication()).removeCurrentUser();
                 Intent intent = new Intent(ProfileActivity.this, LoginActivity.class);
                 startActivity(intent);
                 finish();
